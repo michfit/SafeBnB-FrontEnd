50babe48297ba867ec4a31291d443bae178d3d6c
Noah Santschi-Cooney <noah@santschi-cooney.ch>
2018-09-16 13:40:25 +0100

    Added text to new numbers

--- functions/lib/index.js
+++ functions/lib/index.js
@@ -1,10 +1,12 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+const admin = require("firebase-admin");
 const functions = require("firebase-functions");
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
 //
-exports.helloWorld = functions.https.onRequest((request, response) => {
-    response.send("Hello from Firebase!");
+admin.initializeApp();
+exports.addVote = functions.https.onCall((data, context) => {
+    return admin.database().ref('/votes/').child(data.id).push({ 'test': 'ok' });
 });
 //# sourceMappingURL=index.js.map
--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,10 +1,9 @@
-import * as functions from 'firebase-functions';
 import * as admin from 'firebase-admin';
+import * as functions from 'firebase-functions';
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
 //
-//RIP function
-/* export const addVote = functions.https.onCall((data, context) => {
-  return admin.database().ref('/votes/').ord
+admin.initializeApp();
+export const addVote = functions.https.onCall((data, context) => {
+  return admin.database().ref('/votes/').child(data.id).push({'test': 'ok'});
 });
- */
