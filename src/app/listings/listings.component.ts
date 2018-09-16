d0e705c5171aba26125e602decb1d196b4cb7096
Tywin <tiaven1104@gmail.com>
2018-09-16 02:22:06 -0400

    improve

--- src/app/listings/listings.component.ts
+++ src/app/listings/listings.component.ts
@@ -12,15 +12,34 @@ export class ListingsComponent implements OnInit {
 
   public listings;
   public showed_listings;
+  public AVERAGE_DANGER_LEVEL = 3572.9931034482747;
 
   constructor(db: AngularFireDatabase) {
-    db.list('/airbnb')
+    db.list('/')
     .valueChanges()
     .subscribe(res => {
-        console.log(res)
+        // console.log(res)
         this.listings = res;
+        this.listings.sort(function(a, b){
+          let keyA = a.danger_index
+          let keyB = b.danger_index
+          if(keyA < keyB) return -1;
+          if(keyA > keyB) return 1;
+          return 0;
+      });
         this.showed_listings = this.listings.slice(0, 6);
+        console.log(this.showed_listings);
     })
+  }
+
+  calculate_relative(danger_level) {
+    if(danger_level === 0) {
+      return "Safest Area";
+    }
+    if(danger_level < this.AVERAGE_DANGER_LEVEL) {
+      let num = (danger_level/this.AVERAGE_DANGER_LEVEL*100).toFixed(2);
+      return `${num} % Average Crime Index`;
+    }
   }
 
   ngOnInit() {
